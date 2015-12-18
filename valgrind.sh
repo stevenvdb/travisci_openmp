f1b3a99b3e0c3cc9de7e242233afd15dc339cc02
Steven Vandenbrande <Steven.Vandenbrande@UGent.be>
2015-12-18 11:43:54 +0100

    T0

--- valgrind.sh
+++ valgrind.sh
@@ -5,7 +5,7 @@ export LD_LIBRARY_PATH=./gcc-6.0.0-no-futex-lib/lib64:./gcc-6.0.0-no-futex-lib/l
 for name in main main_corrected
 do
     # Run helgrind
-    valgrind --tool=helgrind --log-file=$name.log --suppression ./$name
+    valgrind --tool=helgrind --log-file=$name.log --suppressions=./gcc-6.0.0-no-futex-lib/valgrind.supp ./$name
     echo $name
     tail -n1 $name.log
 done
